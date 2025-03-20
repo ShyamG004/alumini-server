55235c95ba44860db9774871dc2f7a55fea2814e
Shyam Shunmugam G <mayhsshunmugam@gmail.com>
2025-03-20 08:30:43 +0530

    url modified

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://alumni-job-form.vercel.app',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true, 
 }));
